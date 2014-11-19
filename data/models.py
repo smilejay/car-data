4c2e25c6a6f80a3a59e869ee559c50d84d21f1ea
Jay <smile665@gmail.com>
2014-11-19 14:39:23 +0800

    add data app

---
+++ data/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Acceleration(models.Model):
+    id = models.IntegerField(primary_key=True)
+    device_id = models.CharField(max_length=255)
+    acceleration = models.IntegerField()
+    timestamp = models.DateTimeField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'acceleration'
