78fc507e2ba324abd7a4636e21fe9ec6fee87315
Jay <smile665@gmail.com>
2014-11-19 20:33:49 +0800

    add a demo post API

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, include, url
-import api
+from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns('api.views',
     # Examples:
     # url(r'^$', 'car_data.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^api/', include(api.urls)),
+    url(r'^demo_post/', 'demo_post'),
 )
--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.request import Request
+from lib.response import JSONResponse
 
-# Create your views here.
+
+@csrf_exempt
+def demo_post(request):
+    '''
+    @author: xin.he@dianping.com
+    @note: POST
+    @return: a list of bug count and fixtime information
+    '''
+    if request.method == 'POST':
+        req = Request(request)
+        raw_data = req.DATA
+        device_id = raw_data.get('device_id', default='default')
+        acceleration = raw_data.get('acceleration', default=0)
+        timestamp = raw_data.get('timestamp', default='2014-01-01')
+        data = {'device_id': device_id, 'acceleration': acceleration,
+                'timestamp': timestamp}
+        return JSONResponse(data=data, status=200)
+    else:
+        return JSONResponse({'error': 'It only support HTTP POST method.'},
+            status=200)
