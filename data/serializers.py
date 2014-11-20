3dc8064c90f44d44eccc7f0bb7be3b940678cc1e
Jay <smile665@gmail.com>
2014-11-20 11:22:27 +0800

    add serializer for Acceleration

---
+++ data/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+import data.models as models
+
+
+class AccelerationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Acceleration
+        fields = ('device_id', 'acceleration', 'timestamp')
