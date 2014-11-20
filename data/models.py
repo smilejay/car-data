ef8b2ab29974671bb9672a2f5cf5b46c44ee1448
Jay <smile665@gmail.com>
2014-11-20 23:33:08 +0800

    bugfix: correct the DecimalField

--- data/models.py
+++ data/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Acceleration(models.Model):
     device_id = models.CharField(max_length=255)
-    acceleration = models.DecimalField()
+    acceleration = models.DecimalField(max_digits=8, decimal_places=4)
     timestamp = models.DateTimeField()
     create_time = models.DateTimeField(auto_now_add=True)
     modify_time = models.DateTimeField(auto_now=True)
