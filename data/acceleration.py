f37c9b0b66b889a0a16303cdec5c5aee924e30b0
Jay <smile665@gmail.com>
2014-11-20 11:24:45 +0800

    add acceleration.py

---
+++ data/acceleration.py
@@ -0,0 +1,37 @@
+'''
+Created on Nov 19, 2014
+
+@author: Jay <smile665@gmail.com>
+'''
+from data.serializers import AccelerationSerializer
+from lib.logger import CarLogger
+from datetime import datetime
+
+logger = CarLogger().getLogger()
+
+
+def add(data):
+    '''
+    @summary: add an acceleration record.
+    @parm data: a dict for the Acceleration's serializer.
+    @return: None.
+    '''
+    ret = False
+    new_data = []
+    for i in data:
+        if not isinstance(i['timestamp'], datetime):
+            i['timestamp'] = datetime.strptime(i['timestamp'], '%Y-%m-%d %H:%M:%S')
+        new_data.append(i)
+    a_se = AccelerationSerializer(data=new_data, many=True)
+    if a_se.is_valid():
+        a_se.save()
+        ret = True
+    else:
+        logger.info('invalid data for acceleration table.\n' +
+                    'data: %s' % new_data)
+        ret = False
+    return ret
+
+
+if __name__ == '__main__':
+    pass
